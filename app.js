7959321a8b38b30d38580a07ab1377ad0f5fbc93
Abbassihraf <hraf.abbassi@gmail.com>
2021-04-08 10:38:17 +0100

    Connection avec MongoDB

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const mongoose = require ('mongoose');
+
+
 const app = express();
 require('dotenv').config();
 
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('db connected'))
+.catch(() => console.log('not connect to the databse'))
 
 app.get('/',(req, res) => {
     res.send({message: "Hello"})
 })
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`app is rinning on port ${port}`));
+app.listen(port, () => console.log(`app is running on port ${port}`));

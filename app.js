2e5640041326b6ccec67ecbf1092781c95f6b2fc
Abbassihraf <hraf.abbassi@gmail.com>
2021-04-09 15:14:25 +0100

    Express Validator

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require ('mongoose');
+const expressValidator = require('express-validator');
 
 // Import Routes
 const userRoutes = require('./routes/users');
@@ -19,6 +20,7 @@ mongoose.connect(process.env.DATABASE, {
 
 // Middleware
 app.use(express.json())
+app.use(expressValidator())
 
 // Routes Middleware
 
---
+++ middlewares/userValidator.js
@@ -0,0 +1,23 @@
+exports.userSignUpValidator = (req, res, next) =>{
+    req.check('first_name', 'First name is required !').notEmpty();
+
+    req.check('last_name', 'Last name is required !').notEmpty();
+
+    req.check('email', 'Email is Required !')
+        .notEmpty()
+        .isEmail();
+
+    req.check('password', 'Password is Required !')
+        .notEmpty()
+        .isLength({min:6, max:10})
+        .withMessage('Password must between 6 and 10 Caracters');
+    
+        const errors = req.validationErrors();
+
+        if(errors)
+        {
+            return res.status(400).json(errors)
+        }
+
+        next()
+}

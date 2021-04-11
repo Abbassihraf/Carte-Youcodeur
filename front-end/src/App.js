1842b5a52c2d37ccbdb0cbfd00dd6e15ddd52fd6
Abbassihraf <hraf.abbassi@gmail.com>
2021-04-11 23:43:24 +0000

    Create new Project ReactJS

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () =>  (
+  <div>
+    <h1>Carte Youcodeur</h1>
+    <button class="btn btn-outline-primary">Bonjour</button>
+  </div>
+  
+)
+
+
+export default App;

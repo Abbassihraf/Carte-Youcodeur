a2ea8c52e6622a0be0428f7f4af1903ab94b9323
Abbassihraf <hraf.abbassi@gmail.com>
2021-04-12 12:02:55 +0000

    State User with useState Hooks

--- front-end/src/users/Signup.js
+++ front-end/src/users/Signup.js
@@ -1,29 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from './../core/Layout'
 // import {API_URL} from './../config'
 
-const signup = () => {
+const Signup = () => {
+
+    const [user, setUser] = useState({
+        first_name : '',
+        last_name : '',
+        email : '',
+        password : '',
+    })
+
+    const handleChange = e =>{
+        setUser({...user, [e.target.id]: e.target.value})
+    }
 
     const Form = () => (
         <form >
             <div className="form-group">
                 <label htmlFor="first_name" className="text-muted">First name</label>
-                <input type="text" className="form-control" id="first_name"/>
+                <input onChange={handleChange} type="text" className="form-control" id="first_name"/>
             </div>
             <div className="form-group">
                 <label htmlFor="last_name" className="text-muted">Last name</label>
-                <input  type="text" className="form-control" id="last_name"/>
+                <input onChange={handleChange}  type="text" className="form-control" id="last_name"/>
             </div>
             <div className="form-group">
                 <label htmlFor="email" className="text-muted">Email</label>
-                <input  type="text" className="form-control" id="email"/>
+                <input onChange={handleChange}  type="text" className="form-control" id="email"/>
             </div>
             <div className="form-group">
                 <label htmlFor="password" className="text-muted">Password</label>
-                <input   type="text" className="form-control" id="password"/>
+                <input onChange={handleChange} type="text" className="form-control" id="password"/>
             </div>
             <button className="btn btn-lg btn-block btn-outline-success">Sign Up</button>
-          
+            {JSON.stringify(user)}
         </form>
     )
     return (
@@ -42,4 +53,4 @@ const signup = () => {
     )
 }
 
-export default signup
+export default Signup

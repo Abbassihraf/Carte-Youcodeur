b3eb869ff876e7f027ee1de64162db1649e8ce9b
Abbassihraf <hraf.abbassi@gmail.com>
2021-04-12 10:13:20 +0000

    Routing BrowserRouter, Switch, Route

---
+++ front-end/src/Routes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './core/Home';
+import Signin from './users/Signin';
+import Signup from './users/Signup';
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path= '/' exact component = {Home} />
+                <Route path= '/signin' exact component = {Signin} />
+                <Route path= '/signup' exact component = {Signup} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes
